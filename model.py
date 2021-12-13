cd106464bf6de824dddd51941272ae1cdb8de1ac
Alice Wang <s1965246@ed.ac.uk>
2021-12-13 00:15:00 +0000

    Multimodal-Toolkit

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ def prepare_data(interaction_file: Path = "data/interaction_information.csv",
                  video_semantic_feature_file: Path = "data/features/videos_semantics/video_semantic_features.hdf5",
                  music_feature_folder: Path = "data/audios/musics",
                  subtitle_folder: Path = "data/text/subtitles",
-                 output_merged_file_path: Path = "data/"):
+                 output_merged_file_path: Path = "data/dataset"):
     '''
     :param interaction_file: csv file of interaction information
     :param video_information_file: csv file of video information
@@ -77,7 +77,7 @@ def prepare_data(interaction_file: Path = "data/interaction_information.csv",
     valid_df.to_csv(output_merged_file_path+'/valid.csv')
     test_df.to_csv(output_merged_file_path+'/test.csv')
     print("Done!\nNum examples are: train{}, valid{} and test{}.".format(len(train_df), len(valid_df), len(test_df)))
-
+    #return merged_file
 
 # https://github.com/georgian-io/Multimodal-Toolkit
 @dataclass
@@ -174,13 +174,16 @@ class MultimodalDataTrainingArguments:
 
 
 class MM4SCRDataset(Dataset):
-    def __init__(self, data: pd.DataFrame, tokenizer: BertTokenizer, descriptio_max_token_len: int = 50,
-                 comment_max_token_len : int = 20, subtitle_max_token_len: int = 1000):
+    def __init__(self, data: pd.DataFrame, tokenizer: BertTokenizer, descriptio_max_token_len: int = 30,
+                 comment_max_token_len: int = 10, subtitle_max_token_len: int = 500,
+                 video_style_feature_max_len: int = 100, music_feature_max_len: int = 100):
         self.data = data
         self.tokenizer = tokenizer
         self.descriptio_max_token_len = descriptio_max_token_len
         self.comment_max_token_len = comment_max_token_len
         self.subtitle_max_token_len = subtitle_max_token_len
+        self.video_style_feature_max_len = video_style_feature_max_len
+        self.music_feature_max_len = music_feature_max_len
 
     def __len__(self):
         return len(self.data)
@@ -244,8 +247,46 @@ class MM4SCRDataset(Dataset):
 
 
 if __name__ == '__main__':
+    # pretrained model: bert-base-chinese
     prepare_data()
-    # bert-base-chinese
-    text_cols = ['Title', 'Review Text']
-    category_cols = ['Clothing ID', 'Division Name', 'Department Name', 'Class Name']
-    numerical_cols = ['Rating', 'Age', 'Positive Feedback Count']
+
+    text_cols = ['userLocation', 'videoLocation', 'authorLocation', 'description', 'comment1',
+                 'comment2', 'comment3', 'comment4', 'comment5', 'subtitle']
+    category_cols = ['userID', 'userGender', 'videoID', 'musicID', 'authorID', 'authorGender',
+                     'hot', 'entertaimentHot', 'socialHot', 'challengeHot', 'authorHot']
+    numerical_cols = ['userAge', 'liveScores', 'authorAge', '', '', '', '', '', '']
+    arr_cols = ['video_style_feature', 'video_semantic_feature', 'music_feature', '', '', '']
+
+    column_info_dict = {
+        'text_cols': text_cols,
+        'num_cols': numerical_cols,
+        'cat_cols': category_cols,
+        'label_col': 'finish', # what if multi predictions?
+        'label_list': [1, 0]}
+
+    model_args = ModelArguments(model_name_or_path='bert-base-chinese')
+
+    data_args = MultimodalDataTrainingArguments(
+        data_path='data/dataset',
+        combine_feat_method='gating_on_cat_and_num_feats_then_sum',
+        column_info=column_info_dict,
+        task='classification')
+
+    training_args = TrainingArguments(
+        output_dir="logs/our_model",
+        logging_dir="logs/runs/our_model",
+        overwrite_output_dir=True,
+        do_train=True,
+        do_eval=True,
+        per_device_train_batch_size=32,
+        num_train_epochs=1,
+        evaluate_during_training=True,
+        logging_steps=25,
+        eval_steps=250
+    )
+
+    set_seed(training_args.seed)
+
+    tokenizer_path_or_name = model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path_or_name, cache_dir=model_args.cache_dir)
+
